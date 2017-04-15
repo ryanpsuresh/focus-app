362bfe7cb62297e0fe0264dc59d0c43bd2b6d380
Ryan <ryanpsuresh@gmail.com>
2017-04-14 20:03:13 -0500

    add render for welcome.js

--- components/welcome.js
+++ components/welcome.js
@@ -12,3 +12,43 @@ const images = {
   'world6': require('../assets/giphy6.gif'),
   'world7': require('../assets/giphy7.gif'),
 }
+
+export class Welcome extends React.Component {
+  static navigationOptions = {
+    header: {
+      style: { position: 'absolute' },
+    }
+  };
+  render() {
+    const { navigate } = this.props.navigation; 
+    return (
+       <View style={{flex: 1}}>
+        <Image
+          source={images.world5}
+          resizeMode='cover'
+          style={styles.container}
+        >
+          <View style={styles.titleContianer}>
+            <Text style={styles.title}>
+              Wonders
+            </Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={styles.info}>
+              Discover the wonders of the world in the palm of your hand
+            </Text>
+          </View>
+          <View style={{padding: 30 }}>
+            <Button
+              onPress={() => navigate('Places')}
+              title="Places"
+              style={styles.button}
+              textStyle={styles.buttonText}
+              children={'Start Button'}
+            />
+          </View>
+        </Image>
+      </View>
+    );
+  }
+}
