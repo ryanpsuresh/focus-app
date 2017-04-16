50aa10e4544c8176cdd3b0d6f04a674dc7d640e1
Ryan <ryanpsuresh@gmail.com>
2017-04-16 00:07:55 -0500

    add buton to player.js

--- components/player.js
+++ components/player.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ListView, Text, Image, View, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import Button from 'apsl-react-native-button';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import Sound from 'react-native-sound';
 
 export class MediaPlayer extends React.Component {
@@ -27,41 +28,35 @@ export class MediaPlayer extends React.Component {
 
   render() {
     return (
-    <View style={{padding: 30 }}>
-     <Button
-      onPress={() => {
-        if(this.state.isPlaying) {
-          this.state.isPlaying = false;
-          this.state.sound.pause();
-        }
-        else {
-          this.state.isPlaying = true;
-          this.state.sound.play();
-        }
-       }
-      }
-      title="Main"
-      style={styles.button}
-      textStyle={styles.buttonText}
-      children={'Start Button'}
-     />
+    <View style={styles.background}>
+      <View>
+        <Icon.Button 
+          name = 'play'
+          color = 'black'
+          onPress={() => {
+            if(this.state.isPlaying) {
+              this.state.isPlaying = false;
+              this.state.sound.pause();
+            }
+            else {
+              this.state.isPlaying = true;
+              this.state.sound.play();
+            }
+           }
+          }
+        >
+        </Icon.Button>
+      </View>
     </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  button: {
-    backgroundColor: 'transparent',
-    borderColor: '#EADCDC',
-    padding: 10,
-    borderRadius: 10
-  },
+  background: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  }
 });
